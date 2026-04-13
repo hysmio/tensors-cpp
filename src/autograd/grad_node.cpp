97fce68126be18908ddcf6740dc270c9012e909f
Hayden Sim <hayden@leonardo.ai>
2026-04-13 21:57:16 +1000

    fix: accumulating gradients bug

--- src/autograd/grad_node.cpp
+++ src/autograd/grad_node.cpp
@@ -11,7 +11,7 @@ void AddBackward::backward(Tensor &grad_output) {
         }
         **lhs_ptr->grad += grad_output;
         if (lhs_ptr->grad_fn) {
-            lhs_ptr->grad_fn->backward(**lhs_ptr->grad);
+            lhs_ptr->grad_fn->backward(grad_output);
         }
     }
     if (rhs_ptr->requires_grad) {
@@ -21,7 +21,7 @@ void AddBackward::backward(Tensor &grad_output) {
         }
         **rhs_ptr->grad += grad_output;
         if (rhs_ptr->grad_fn) {
-            rhs_ptr->grad_fn->backward(**rhs_ptr->grad);
+            rhs_ptr->grad_fn->backward(grad_output);
         }
     }
 }
@@ -35,7 +35,7 @@ void SubBackward::backward(Tensor &grad_output) {
         }
         **lhs_ptr->grad += grad_output;
         if (lhs_ptr->grad_fn) {
-            lhs_ptr->grad_fn->backward(**lhs_ptr->grad);
+            lhs_ptr->grad_fn->backward(grad_output);
         }
     }
     if (rhs_ptr->requires_grad) {
@@ -44,20 +44,21 @@ void SubBackward::backward(Tensor &grad_output) {
             (*rhs_ptr->grad)->zero();
         }
 
+        // Negate gradient for rhs: d(a-b)/db = -1
+        Tensor neg_grad(grad_output.shape, false, rhs_ptr->device);
         switch (rhs_ptr->device) {
         case Device::CPU:
-            // Negate gradient for rhs: d(a-b)/db = -1, accumulate the negated value
             for (uint32_t i = 0; i < rhs_ptr->size; i++) {
-                (*rhs_ptr->grad)->data()[i] -= grad_output.data()[i];
+                neg_grad.data()[i] = -grad_output.data()[i];
             }
             break;
         case Device::CUDA:
-            launch_vec_subtract((*rhs_ptr->grad)->data(), grad_output.data(),
-                                (*rhs_ptr->grad)->data(), rhs_ptr->size);
+            launch_negate(grad_output.data(), neg_grad.data(), rhs_ptr->size);
             break;
         }
+        **rhs_ptr->grad += neg_grad;
         if (rhs_ptr->grad_fn) {
-            rhs_ptr->grad_fn->backward(**rhs_ptr->grad);
+            rhs_ptr->grad_fn->backward(neg_grad);
         }
     }
 }
@@ -75,7 +76,7 @@ void MulBackward::backward(Tensor &grad_output) {
         auto grad = grad_output * (*rhs_ptr);
         **lhs_ptr->grad += grad;
         if (lhs_ptr->grad_fn) {
-            lhs_ptr->grad_fn->backward(**lhs_ptr->grad);
+            lhs_ptr->grad_fn->backward(grad);
         }
     }
 
@@ -87,7 +88,7 @@ void MulBackward::backward(Tensor &grad_output) {
         auto grad = grad_output * (*lhs_ptr);
         **rhs_ptr->grad += grad;
         if (rhs_ptr->grad_fn) {
-            rhs_ptr->grad_fn->backward(**rhs_ptr->grad);
+            rhs_ptr->grad_fn->backward(grad);
         }
     }
 }
@@ -104,7 +105,7 @@ void DivBackward::backward(Tensor &grad_output) {
         auto grad = grad_output * (*rhs_ptr);
         **lhs_ptr->grad += grad;
         if (lhs_ptr->grad_fn) {
-            lhs_ptr->grad_fn->backward(**lhs_ptr->grad);
+            lhs_ptr->grad_fn->backward(grad);
         }
     }
 
@@ -116,7 +117,7 @@ void DivBackward::backward(Tensor &grad_output) {
         auto grad = grad_output * (*lhs_ptr);
         **rhs_ptr->grad += grad;
         if (rhs_ptr->grad_fn) {
-            rhs_ptr->grad_fn->backward(**rhs_ptr->grad);
+            rhs_ptr->grad_fn->backward(grad);
         }
     }
 }
@@ -135,7 +136,7 @@ void MatmulBackward::backward(Tensor &grad_output) {
         auto grad = matmul(grad_output, rhs_transposed);
         **lhs_ptr->grad += grad;
         if (lhs_ptr->grad_fn) {
-            lhs_ptr->grad_fn->backward(**lhs_ptr->grad);
+            lhs_ptr->grad_fn->backward(grad);
         }
     }
 
@@ -148,7 +149,7 @@ void MatmulBackward::backward(Tensor &grad_output) {
         auto grad = matmul(lhs_transposed, grad_output);
         **rhs_ptr->grad += grad;
         if (rhs_ptr->grad_fn) {
-            rhs_ptr->grad_fn->backward(**rhs_ptr->grad);
+            rhs_ptr->grad_fn->backward(grad);
         }
     }
 }
@@ -167,7 +168,7 @@ void LinearBackward::backward(Tensor &grad_output) {
         auto grad = matmul(grad_output, *weights_ptr);
         **input_ptr->grad += grad;
         if (input_ptr->grad_fn) {
-            input_ptr->grad_fn->backward(**input_ptr->grad);
+            input_ptr->grad_fn->backward(grad);
         }
     }
 
@@ -182,7 +183,7 @@ void LinearBackward::backward(Tensor &grad_output) {
         auto grad = matmul(grad_transposed, *input_ptr);
         **weights_ptr->grad += grad;
         if (weights_ptr->grad_fn) {
-            weights_ptr->grad_fn->backward(**weights_ptr->grad);
+            weights_ptr->grad_fn->backward(grad);
         }
     }
 }
@@ -196,21 +197,25 @@ void SumBackward::backward(Tensor &grad_output) {
             *input_ptr->grad = std::make_shared<Tensor>(input_ptr->shape, false, input_ptr->device);
             (*input_ptr->grad)->zero();
         }
+        // Broadcast scalar grad_output to all elements
+        Tensor broadcast_grad(input_ptr->shape, false, input_ptr->device);
         switch (input_ptr->device) {
         case Device::CPU: {
             float grad_val = grad_output.data()[0];
             for (uint32_t i = 0; i < input_ptr->size; i++) {
-                (*input_ptr->grad)->data()[i] += grad_val;
+                broadcast_grad.data()[i] = grad_val;
             }
             break;
         }
         case Device::CUDA:
-            launch_scalar_addp((*input_ptr->grad)->data(), grad_output.data(),
-                               (*input_ptr->grad)->data(), input_ptr->size);
+            launch_fill_value(broadcast_grad.data(), 0.0f, input_ptr->size);
+            launch_scalar_addp(broadcast_grad.data(), grad_output.data(), broadcast_grad.data(),
+                               input_ptr->size);
             break;
         }
+        **input_ptr->grad += broadcast_grad;
         if (input_ptr->grad_fn) {
-            input_ptr->grad_fn->backward(**input_ptr->grad);
+            input_ptr->grad_fn->backward(broadcast_grad);
         }
     }
 }
@@ -227,13 +232,10 @@ void DivScalarBackward::backward(Tensor &grad_output) {
         }
 
         Tensor grad_output_scaled = grad_output / scalar;
-
-        for (uint32_t i = 0; i < input_ptr->size; i++) {
-            (**input_ptr->grad) += grad_output_scaled;
-        }
+        **input_ptr->grad += grad_output_scaled;
 
         if (input_ptr->grad_fn) {
-            input_ptr->grad_fn->backward(**input_ptr->grad);
+            input_ptr->grad_fn->backward(grad_output_scaled);
         }
     }
 }
@@ -247,13 +249,14 @@ void ReluBackward::backward(Tensor &grad_output) {
             *input_ptr->grad = std::make_shared<Tensor>(input_ptr->shape, false, input_ptr->device);
             (*input_ptr->grad)->zero();
         }
+        Tensor local_grad(input_ptr->shape, false, input_ptr->device);
         for (uint32_t i = 0; i < input_ptr->size; i++) {
-            // this is just leaky relu, which i think is also identical to gelu idk
             float grad_mult = input_ptr->data()[i] > 0 ? 1.0f : leak;
-            (*input_ptr->grad)->data()[i] += grad_output.data()[i] * grad_mult;
+            local_grad.data()[i] = grad_output.data()[i] * grad_mult;
+            (*input_ptr->grad)->data()[i] += local_grad.data()[i];
         }
         if (input_ptr->grad_fn) {
-            input_ptr->grad_fn->backward(**input_ptr->grad);
+            input_ptr->grad_fn->backward(local_grad);
         }
     }
 }
@@ -271,11 +274,11 @@ void TanhBackward::backward(Tensor &grad_output) {
 
         auto squared = *output_ptr * *output_ptr;
         auto one_minus_squared = -squared + 1.0f;
-        auto grad_mult = grad_output * one_minus_squared;
-        *(*input_ptr->grad) += grad_mult;
+        auto local_grad = grad_output * one_minus_squared;
+        *(*input_ptr->grad) += local_grad;
 
         if (input_ptr->grad_fn) {
-            input_ptr->grad_fn->backward(**input_ptr->grad);
+            input_ptr->grad_fn->backward(local_grad);
         }
     }
 }
